74f47e796a7702c66777cce2c4891e96d5350d8b
Johan Wirén <johan.wiren.se@gmail.com>
2021-01-02 04:04:07 +0100

    Day 23 - part 2. Still slow (5 mins)

--- src/adventofcode_2020/day_23.clj
+++ src/adventofcode_2020/day_23.clj
@@ -1,52 +1,57 @@
 (ns adventofcode-2020.day-23
-  (:require [adventofcode-2020.util :refer [t tt]]
-            [clojure.test :as t]))
+  (:require [clojure.test :as t]))
 
 (def reference-input [3 8 9 1 2 5 4 6 7])
 
 (def input [9 6 3 2 7 5 4 8 1])
 
 (defn parse-input [in]
-  (let [curr (first in)]
-    {:nexts (zipmap in (conj (vec (rest in)) curr))
-     :curr curr}))
+  (let [nexts (make-array Integer/TYPE (inc (count in)))
+        curr (first in)]
+    (run! (fn [[k v]]
+            (aset-int nexts k v))
+          (zipmap in (conj (vec (rest in))
+                           curr)))
+    [(first in) nexts (apply max in)]))
 
 (defn parse-input-2 [in]
-  (let [curr (first in)
-        m (apply max in)
-        all (concat in (range (inc m) 1001))]
-    {:nexts (zipmap all (conj (vec (rest all)) curr))
-     :curr curr}))
+  (let [nexts (into-array Integer/TYPE (range 1 1000002))]
+    (run! (fn [[k v]]
+            (aset-int nexts k v))
+          (zipmap in (conj (vec (rest in))
+                           (inc (count in)))))
+    (aset nexts 1000000 (first in))
+    [(first in) nexts 1000000]))
 
-(defn- pick-up [nexts curr]
+(defn pick-up [nexts curr]
   (->> curr
-       (iterate nexts)
+       (iterate (partial aget nexts))
        (drop 1)
        (take 3)))
 
-(defn- dest-cup [remaining-cups curr]
+(defn dest-cup [nexts picked-up curr max]
   (or (->> (iterate dec (dec curr))
            (take-while pos?)
-           (filter (set remaining-cups))
+           (filter (partial aget nexts))
+           (remove (set picked-up))
            (first))
-      (apply max remaining-cups)))
+      max))
 
-(defn move [{:keys [curr nexts]}]
+(defn move [[curr nexts max]]
   (let [picked-up (pick-up nexts curr)
-        remaining-cups (->> (keys nexts)
-                            (remove (set picked-up)))
-        dest-cup (dest-cup remaining-cups curr)
+        dest-cup (dest-cup nexts picked-up curr max)
         last-picked-up (last picked-up)
-        new-nexts (-> nexts
-                      (assoc curr (nexts last-picked-up))
-                      (assoc dest-cup (first picked-up))
-                      (assoc last-picked-up (nexts dest-cup)))]
-    {:nexts new-nexts
-     :curr (new-nexts curr)}))
+        new-curr (aget nexts last-picked-up)
+        new-dest-cup (first picked-up)
+        new-last-picked-up (aget nexts dest-cup)]
+    (aset-int nexts curr new-curr)
+    (aset-int nexts dest-cup new-dest-cup)
+    (aset-int nexts last-picked-up new-last-picked-up)
+    [(aget nexts curr) nexts max]))
 
 (defn- right-of [nexts cup]
-  (->> (nexts cup)
-       (iterate nexts)
+  (->> (aget nexts cup)
+       (iterate (partial aget nexts))
        (take 8)
        (apply str)
        (Integer/parseInt)))
@@ -57,28 +62,21 @@
                    (iterate move)
                    (drop 100)
                    (first)
-                   (:nexts))]
+                   (second))]
     (right-of nexts 1)))
 
 (defn part-2-solver [in]
   (let [game (parse-input-2 in)
         nexts (->> game
                    (iterate move)
-                   (drop 10000)
+                   (drop 10000000)
                    (first)
-                   (:nexts))]
-    (nexts 1)))
-
-(comment
-
-  (part-1-solver input)
-  (part-2-solver input)
-
-  )
+                   (second))]
+    (* (aget nexts 1)
+       (aget nexts (aget nexts 1)))))
 
 (t/deftest part-1-test
-  (t/is (= 67384529 (part-1-solver reference-input)))
-  (t/is (= 97632548 (time (part-1-solver input)))))
+  (t/is (= 97632548 (part-1-solver input))))
 
 (t/deftest part-2-test
-  (t/is (= :FIXME (part-2-solver reference-input))))
+  (t/is (= 412990492266 (time (part-2-solver reference-input)))))
