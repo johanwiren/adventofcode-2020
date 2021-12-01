48d7c6c31bf0946971eda959c1979fdd8922b031
Johan Wirén <johan.wiren@klarna.com>
2021-12-01 10:50:17 +0100

    2021 day 01 - transducers

--- src/adventofcode_2021/day_01.clj
+++ src/adventofcode_2021/day_01.clj
@@ -5,36 +5,53 @@
 
 (def input (-> "2021/day_01.txt" (io/resource) (io/reader) (line-seq)))
 
-
 (defn parse-input [input]
   (map edn/read-string input))
 
+(defn add-diff [rf]
+  (let [prev (volatile! nil)]
+    (fn
+      ([] (rf))
+      ([res] (rf res))
+      ([res item]
+       (let [diff (if @prev
+                    (- item @prev)
+                    0)]
+         (vreset! prev item)
+         (rf res [item diff]))))))
+
 (defn- part-1 [in]
-  (->> (drop 1 in)
-       (reduce (fn [acc item]
-                 (conj acc [item (- item (first (last acc)))]))
-               [[(first in) 0]])
-       (filter (comp pos? second))
-       count))
+  (->> in
+       (sequence (comp add-diff
+                       (filter (comp pos? second))))
+       (count)))
 
 (defn part-1-solver [input]
   (let [in (parse-input input)]
     (part-1 in)))
 
+(defn sliding-window
+  ([n]
+   (let [window (volatile! [])]
+     (fn [rf]
+       (fn
+         ([] (rf))
+         ([res] (rf res))
+         ([res item]
+          (if (= n (count @window))
+            (do
+              (vreset! window (-> @window (subvec 1) (conj item)))
+              (rf res @window))
+            (vswap! window conj item)))))))
+  ([n xs]
+   (sequence (sliding-window n) xs)))
+
 (defn part-2-solver [input]
   (let [in (parse-input input)]
     (->> in
-         (reduce (fn [acc item]
-                   (if (<= 3 (count acc))
-                     (conj acc [item (->> acc
-                                          (take-last 3)
-                                          (map first)
-                                          (reduce +))])
-                     (conj acc [item 0])))
-                 [])
-         (map second)
+         (sequence (comp (sliding-window 3)
+                         (map (partial apply +))))
          (part-1))))
-
 
 (comment
   (part-1-solver input)
@@ -42,8 +59,6 @@
   (part-2-solver input)
 
   )
-
-
 
 (t/deftest part-1-test
   (t/is (= 1228 (part-1-solver input))))
