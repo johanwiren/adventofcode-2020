b4b8fe63410e3f0efab568686fe289d08983e53e
Johan Wirén <johan.wiren@klarna.com>
2021-12-01 13:08:47 +0100

    2021 day 1 - sliding windows

--- src/adventofcode_2021/day_01.clj
+++ src/adventofcode_2021/day_01.clj
@@ -1,64 +1,20 @@
 (ns adventofcode-2021.day-01
-  (:require [clojure.java.io :as io]
-            [clojure.edn :as edn]
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
             [clojure.test :as t]))
 
-(def input (-> "2021/day_01.txt" (io/resource) (io/reader) (line-seq)))
+(def input (->> "2021/day_01.txt" (io/resource) (io/reader) (line-seq) (map edn/read-string)))
 
-(defn parse-input [input]
-  (map edn/read-string input))
-
-(defn add-diff [rf]
-  (let [prev (volatile! nil)]
-    (fn
-      ([] (rf))
-      ([res] (rf res))
-      ([res item]
-       (let [diff (if @prev
-                    (- item @prev)
-                    0)]
-         (vreset! prev item)
-         (rf res [item diff]))))))
-
-(defn- part-1 [in]
-  (->> in
-       (sequence (comp add-diff
-                       (filter (comp pos? second))))
+(defn solver [window-size input]
+  (->> input
+       (partition window-size 1)
+       (sequence (comp (map (juxt first last))
+                       (filter (partial apply <))))
        (count)))
 
-(defn part-1-solver [input]
-  (let [in (parse-input input)]
-    (part-1 in)))
+(def part-1-solver (partial solver 2))
 
-(defn sliding-window
-  ([n]
-   (let [window (volatile! [])]
-     (fn [rf]
-       (fn
-         ([] (rf))
-         ([res] (rf res))
-         ([res item]
-          (if (= n (count @window))
-            (do
-              (vreset! window (-> @window (subvec 1) (conj item)))
-              (rf res @window))
-            (vswap! window conj item)))))))
-  ([n xs]
-   (sequence (sliding-window n) xs)))
-
-(defn part-2-solver [input]
-  (let [in (parse-input input)]
-    (->> in
-         (sequence (comp (sliding-window 3)
-                         (map (partial apply +))))
-         (part-1))))
-
-(comment
-  (part-1-solver input)
-
-  (part-2-solver input)
-
-  )
+(def part-2-solver (partial solver 4))
 
 (t/deftest part-1-test
   (t/is (= 1228 (part-1-solver input))))
