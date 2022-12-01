7a6097764a44f24e97d1a80eb91b9442c0eaf487
Johan Wirén <johan@johanwiren.se>
2022-12-01 06:21:54 +0100

    Day 01

---
+++ src/adventofcode_2022/day_01.clj
@@ -0,0 +1,31 @@
+(ns adventofcode-2022.day-01
+  (:require [adventofcode-2022.utils :as u]
+            [clojure.test :as t]))
+
+(def input (u/line-seq-input *ns*))
+
+(defn parse-input [input]
+  (->> input
+       (map parse-long)
+       (partition-by nil?)
+       (remove #{[nil]})))
+
+(defn part-1-solver [input]
+  (->> input
+       (parse-input)
+       (map (partial reduce +))
+       (apply max)))
+
+(defn part-2-solver [input]
+  (->> input
+       (parse-input)
+       (map (partial reduce +))
+       (sort)
+       (take-last 3)
+       (reduce +)))
+
+(t/deftest part-1-test
+  (t/is (= 66306 (time (part-1-solver input)))))
+
+(t/deftest part-2-test
+  (t/is (= 195292 (time (part-2-solver input)))))
