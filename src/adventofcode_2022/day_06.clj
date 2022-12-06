87440cdbae1342af0bebac2155bcd1c686651b80
Johan Wirén <johan@johanwiren.se>
2022-12-06 06:41:46 +0100

    Day 06

---
+++ src/adventofcode_2022/day_06.clj
@@ -0,0 +1,25 @@
+(ns adventofcode-2022.day-06
+  (:require [adventofcode-2022.utils :as u]
+            [clojure.test :as t]))
+
+(def input (-> (u/line-seq-input *ns*)
+               (first)
+               (vec)))
+
+(defn find-marker-pos [vec start-pos marker-size]
+  (loop [pos start-pos]
+    (if (apply distinct? (subvec vec pos (+ pos marker-size)))
+      (+ pos marker-size)
+      (recur (inc pos)))))
+
+(defn part-1-solver [input]
+  (find-marker-pos input 0 4))
+
+(defn part-2-solver [input]
+  (find-marker-pos input (part-1-solver input) 14))
+
+(t/deftest part-1-test
+  (t/is (= 1093 (time (part-1-solver input)))))
+
+(t/deftest part-2-test
+  (t/is (= 3534 (time (part-2-solver input)))))
