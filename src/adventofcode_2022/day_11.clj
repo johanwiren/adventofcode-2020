5f85685e08dd5119cfdef738cb977817121b6e1e
Johan Wirén <johan@johanwiren.se>
2022-12-11 21:55:28 +0100

    Day 11 - Cleanup

--- src/adventofcode_2022/day_11.clj
+++ src/adventofcode_2022/day_11.clj
@@ -6,15 +6,11 @@
 (def input (u/line-seq-input *ns*))
 
 (defn make-op-fn [op-line]
-  (let [[arg1 f arg2] (take-last 3 (str/split op-line #" "))
-        sym           (case f "+" + "*" *)]
-    (cond
-      (= "old" arg1 arg2)
-      (fn [old]
-        (sym old old))
-      :else
-      (fn [old]
-        (sym old (parse-long arg2))))))
+  (let [[arg1 op arg2] (take-last 3 (str/split op-line #" "))
+        f              (case op "+" + "*" *)]
+    (if (= "old" arg1 arg2)
+      #(f % %)
+      #(f (parse-long arg2) %))))
 
 (defn parse-monkey [monkey]
   (let [[_ item-line op-line test-line & rules]
@@ -23,7 +19,7 @@
         op-fn                  (make-op-fn op-line)
         divisor                (parse-long (last (re-seq #"\d+" test-line)))
         test-fn                (comp zero? #(rem % divisor))
-        items                  (mapv parse-long (re-seq #"\d+" item-line))
+        items                  (map parse-long (re-seq #"\d+" item-line))
         [case-true case-false] (map parse-long (re-seq #"\d+" (apply str rules)))]
     {:items     items
      :inspected 0
@@ -45,10 +41,10 @@
               (reduce (fn [monkeys item]
                         (let [val           (-> item op-fn worry-strategy)
                               target-monkey (if (test-fn val) case-true case-false)]
-                          (update-in monkeys [target-monkey :items] conj val)))
+                          (update monkeys target-monkey update :items #(cons val %))))
                       (-> monkeys
-                          (assoc-in [m :items] [])
-                          (update-in [m :inspected] + (count items)))
+                          (update m assoc :items (list))
+                          (update m update :inspected + (count items)))
                       items)))
           monkeys
           (range (count monkeys))))
@@ -68,8 +64,8 @@
 
 (defn part-2-solver [input]
   (let [monkeys        (parse-input input)
-        gcd            (apply * (map :divisor monkeys))
-        worry-strategy #(mod % gcd)]
+        multiplier     (apply * (map :divisor monkeys))
+        worry-strategy #(mod % multiplier)]
     (solve monkeys worry-strategy 10000)))
 
 (t/deftest part-1-test
