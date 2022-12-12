e4e46b5ca926236e7e8ffd5664d1c5d35bc93986
Johan Wirén <johan@johanwiren.se>
2022-12-12 22:13:28 +0100

    Simple benchmark

--- src/adventofcode_2022/utils.clj
+++ src/adventofcode_2022/utils.clj
@@ -9,3 +9,19 @@
            (io/resource)
            (io/reader)
            (line-seq)))
+
+(defn benchmark [{:keys [day n]
+                  :or   {n 1000}}]
+  (doseq [day (if day [day] (range 1 13))]
+    (let [ns    (symbol (str "adventofcode-2022.day-" (format "%02d" day)))
+          _     (require ns)
+          p1    (ns-resolve ns 'part-1-solver)
+          p2    (ns-resolve ns 'part-2-solver)
+          input @(ns-resolve ns (symbol "input"))]
+
+      (println (format "== Day %s Part 1 - %s times ==" day n))
+      (time (dotimes [_ n]
+              (p1 input)))
+      (println (format "== Day %s Part 2 - %s times ==" day n))
+      (time (dotimes [_ n]
+              (p2 input))))))
