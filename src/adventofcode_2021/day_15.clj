f102b54b0f64dfd0de72ded71c5a131f1d39261c
Johan Wirén <johan@johanwiren.se>
2022-12-12 12:21:54 +0100

    Day 15 - Fully mutating a*, still not faster

--- src/adventofcode_2021/day_15.clj
+++ src/adventofcode_2021/day_15.clj
@@ -1,6 +1,7 @@
 (ns adventofcode-2021.day-15
   (:require [clojure.java.io :as io]
-            [clojure.test :as t]))
+            [clojure.test :as t])
+  (:import (java.util HashMap)))
 
 (def input (->> "2021/day_15.txt"
                 (io/resource)
@@ -18,45 +19,43 @@
   [[(dec x) y] [(inc x) y] [x (dec y)] [x (inc y)]])
 
 (defn a-star [vs source target neighbours-fn len-fn h]
-  (let [inf Double/POSITIVE_INFINITY]
-    (loop [g-score (transient {source 0})
-           came-from (transient {})
-           u source
-           Q (let [q (java.util.PriorityQueue. (count vs) (fn [x y]
-                                                            (- (h y) (h x))))]
-               (.add q source)
-               q)]
-      (if (or (= u target) (zero? (.size Q)) (nil? (get g-score u)))
-        {:g-score (persistent! g-score)
-         :came-from (persistent! came-from)}
-        (let [[g-score came-from]
-              (reduce (fn [[g-score came-from :as acc] v]
-                        (let [alt (+ (get g-score u inf) (len-fn u v))]
-                          (if (< alt (get g-score v inf))
-                            (do
-                              (.add Q v)
-                              [(assoc! g-score v alt)
-                               (assoc! came-from v u)])
-                            acc)))
-                      [g-score came-from]
-                      (neighbours-fn u))]
-          (.remove Q u)
-          (recur g-score came-from (.peek Q) Q))))))
+  (let [inf       Double/POSITIVE_INFINITY
+        g-score   (HashMap.)
+        came-from (HashMap.)
+        Q         (let [q (java.util.PriorityQueue. (count vs) (fn [x y]
+                                                                 (- (h y) (h x))))]
+                    (.add q source)
+                    q)]
+    (.put g-score source 0)
+    (loop []
+      (let [u (.peek Q)]
+        (if (or (= u target) (zero? (.size Q)) (nil? (get g-score u)))
+          {:g-score   (into {} g-score)
+           :came-from (into {} came-from)}
+          (do
+            (doseq [v (neighbours-fn u)]
+              (let [alt (+ (get g-score u inf) (len-fn u v))]
+                (when (< alt (get g-score v inf))
+                  (.add Q v)
+                  (.put g-score v alt)
+                  (.put came-from v u))))
+            (.remove Q u)
+            (recur)))))))
 
 (defn least-cost [input]
-  (let [es input
-        square-size (dec (count (first es)))
-        goal [square-size square-size]
-        vs (for [x (range (inc square-size))
-                 y (range (inc square-size))]
-             [x y])
-        neigh-fn (fn [v]
-                   (filter (fn [[x y]]
-                             (and (<= 0 x square-size)
-                                  (<= 0 y square-size)))
-                           (neighbours v)))
-        h (fn [v]
-            (apply + (map - goal v)))
+  (let [es                  input
+        square-size         (dec (count (first es)))
+        goal                [square-size square-size]
+        vs                  (for [x (range (inc square-size))
+                                  y (range (inc square-size))]
+                              [x y])
+        neigh-fn            (fn [v]
+                              (filter (fn [[x y]]
+                                        (and (<= 0 x square-size)
+                                             (<= 0 y square-size)))
+                                      (neighbours v)))
+        h                   (fn [v]
+                              (apply + (map - goal v)))
         {:keys [came-from]} (a-star vs [0 0] goal neigh-fn (fn [_u v] (get-in es v)) h)]
     (->> (iterate came-from goal)
          (take-while identity)
