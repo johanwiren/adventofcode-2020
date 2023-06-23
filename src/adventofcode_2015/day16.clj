60c010cd1db5ad400c07b49b75fbd26f24bc1d8c
Johan Wirén <johan@johanwiren.se>
2023-06-23 18:27:22 +0200

    2015 day 16

---
+++ src/adventofcode_2015/day16.clj
@@ -0,0 +1,62 @@
+(ns adventofcode-2015.day16
+  (:require [utils :as u]
+            [clojure.string :as str]
+            [clojure.test :as t]))
+
+(def input (u/line-seq-input *ns*))
+
+(defn parse-line [line]
+  (let [kws (-> line
+                (str/replace #"[:,]" "")
+                (str/split #" "))]
+    (-> (apply hash-map kws)
+        (update-vals parse-long)
+        (update-keys keyword))))
+
+(defn parse-input [input]
+  (map parse-line input))
+
+(def my-sue {:children 3
+             :cats 7
+             :samoyeds 2
+             :pomeranians 3
+             :akitas 0
+             :vizslas 0
+             :goldfish 5
+             :trees 3
+             :cars 2
+             :perfumes 1})
+
+(defn is-my-sue? [sue]
+  (every? (fn [[k v]]
+            (= v (k my-sue)))
+          (dissoc sue :Sue)))
+
+(defn part-1-solver [input]
+  (->> (parse-input input)
+       (filter is-my-sue?)
+       (first)
+       :Sue))
+
+(defn sue-pred [k]
+  (case k
+    (:cats :trees) >
+    (:pomeranians :goldfish) <
+    =))
+
+(defn is-my-sue-p2? [sue]
+  (every? (fn [[k v]]
+            ((sue-pred k) v (k my-sue)))
+          (dissoc sue :Sue)))
+
+(defn part-2-solver [input]
+  (->> (parse-input input)
+       (filter is-my-sue-p2?)
+       (first)
+       :Sue))
+
+(t/deftest part-1-solver-test
+  (t/is (= 373 (part-1-solver input))))
+
+(t/deftest part-2-solver-test
+  (t/is (= 260 (part-2-solver input))))
