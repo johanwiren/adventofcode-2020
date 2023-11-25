6fad4eb3b868829db82f563aba75a96d84a9ccba
Johan Wirén <johan@pitch.io>
2023-11-25 16:35:52 +0100

    2017 day 02

---
+++ src/adventofcode_2017/day_02.clj
@@ -0,0 +1,43 @@
+(ns adventofcode-2017.day-02
+  (:require [adventofcode-2022.utils :as u]
+            [clojure.test :as t]
+            [clojure.string :as str]))
+
+(def input (u/line-seq-input *ns*))
+
+(defn parse-line [line]
+  (->> (str/split line #"\t")
+       (mapv parse-long)))
+
+(defn parse-input [input]
+  (mapv parse-line input))
+
+(defn row-chksum [row]
+  (let [sorted (sort row)
+        min (first sorted)
+        max (last sorted)]
+    (- max min)))
+
+(defn part-1-solver [input]
+  (let [rows (parse-input input)]
+    (reduce + (map row-chksum rows))))
+
+(defn divisors [row]
+  (for [x (range (count row))
+        y (range (count row))
+        :let [n1 (get row x)
+              n2 (get row y)]
+        :when (and (not= x y)
+                   (zero? (rem n1 n2)))]
+    [n1 n2]))
+
+(defn part-2-solver [input]
+  (->> (parse-input input)
+       (map (comp (partial apply /) first divisors))
+       (reduce +)))
+
+(t/deftest part-1-test
+  (t/is (= 54426 (part-1-solver input))))
+
+(t/deftest part-2-test
+  (t/is (= 333 (part-2-solver input))))
