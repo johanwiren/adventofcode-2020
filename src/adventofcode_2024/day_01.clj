3a5a257d1a3ba7a19625c554fccbb39667a4c0c1
Johan Wirén <johan@johanwiren.se>
2024-12-01 16:54:29 +0100

    Removes generated comments

--- src/adventofcode_2024/day_01.clj
+++ src/adventofcode_2024/day_01.clj
@@ -10,27 +10,17 @@
   (map #(map parse-long (str/split % #" +")) input))
 
 (defn part-1-solver [input]
-  ;; Parse the input string into a sequence of vectors
   (->> (parse input)
-       ;; Transpose the sequence of vectors to swap rows with columns
        (u/transpose)
-       ;; Sort each column in ascending order
        (map sort)
-       ;; For each pair of elements from corresponding columns,
-       ;; compute the absolute difference and negate it
        (apply map (comp abs - ))
-       ;; Sum up all the negated differences to get the final result
        (reduce +)))
 
 (defn part-2-solver [input]
   (let [[xs ys] (u/transpose (parse input))
-        ;; Count the frequency of each character at each position
         freqs (frequencies ys)]
     (->> xs
-         ;; For each list of characters, multiply the length by the
-         ;; frequency of the first character in that list
          (map #(* % (get freqs % 0)))
-         ;; Sum all the scores to get the final result
          (reduce +))))
 
 (t/deftest part-1-test
